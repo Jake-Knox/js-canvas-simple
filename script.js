879224357fe3f3dfbbd20fe9fb5722cf80c7c22f
jake-knox- <jakeknox11@hotmail.co.uk>
2023-07-01 12:54:24 +0100

    brick class, middle mouse for brick, no collisions yet.

--- script.js
+++ script.js
@@ -28,13 +28,43 @@ class Ball {
     }
 }
 
+class Brick {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = getRandomHexColor();
+    }
+    
+    draw() {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const balls = [];
+const bricks = [];
+
 
 canvas.addEventListener("click", (event) => {
     addBall(event);
+    console.log("new ball")
 });
+canvas.addEventListener("mousedown", event => {
+    if (event.button === 1) {
+      addBrick(event);
+      console.log("new brick")
+    }
+});
+  
 
 
 const addBall = (event) => {
@@ -49,6 +79,19 @@ const addBall = (event) => {
     balls.push(newBall);
 }
 
+function addBrick(event) {
+    const rect = canvas.getBoundingClientRect();
+
+    const width = 80;
+    const height = 20;
+
+    const x = event.clientX - rect.left - width / 2;
+    const y = event.clientY - rect.top - height / 2;    
+  
+    const newBrick = new Brick(x, y, width, height);
+    bricks.push(newBrick);
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   
@@ -56,12 +99,24 @@ function draw() {
       ball.draw();
       ball.update();
     });
-  
+
+   bricks.forEach(brick => {
+    brick.draw();
+  });
     requestAnimationFrame(draw);
 }
 
 function getRandomNumber(min, max) {
     return Math.random() * (max - min) + min;
-  }
+}
+
+  function getRandomHexColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 draw();
