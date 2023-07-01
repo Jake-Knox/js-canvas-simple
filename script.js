3e83b9cf06eae3ce095ead90b1b08db1c1593fbe
jake-knox- <jakeknox11@hotmail.co.uk>
2023-07-01 12:34:09 +0100

    class for balls

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
+
+
+class Ball {
+    constructor(x, y, radius, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.dx = dx;
+        this.dy = dy;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+    update() {
+        this.x += this.dx;
+        this.y += this.dy;
+    
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+          this.dx = -this.dx;
+        }
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+          this.dy = -this.dy;
+        }
+    }
+}
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+const balls = [];
+
+canvas.addEventListener("click", (event) => {
+    addBall(event);
+});
 
 
-const ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    radius: 10,
-    dx: 2, // velocity in the x direction
-    dy: -2 // velocity in the y direction
-};
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+const addBall = (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+  
+    const newBall = new Ball(x, y, 10, 2, -2);
+    balls.push(newBall);
 }
-  
-function updateBall() {
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-  
-    // Check for wall collisions
-    if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
-      ball.dx = -ball.dx; // reverse the velocity in x direction
-    }
-    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
-      ball.dy = -ball.dy; // reverse the velocity in y direction
-    }
-  }
-  
-  function draw() {
+
+function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    updateBall();
+  
+    balls.forEach(ball => {
+      ball.draw();
+      ball.update();
+    });
+  
     requestAnimationFrame(draw);
 }
 
 draw();
-
-  
